69a04233cf41334fbf6a82e3980f21dafd94af13
Ewan Gilchrist <egilchrist@kangaroot.net>
2024-12-12 09:20:32 +0100

    Add some stolen config from https://github.com/viperML ... thanks!

--- nvim-conf/lua/eagle/treesitter.lua
+++ nvim-conf/lua/eagle/treesitter.lua
@@ -16,3 +16,26 @@ require("nvim-treesitter.configs").setup {
     },
   },
 }
+
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+
+vim.opt.smartindent = true
+
+-- Don't acidentally run these
+vim.api.nvim_del_user_command("TSUpdate")
+vim.api.nvim_del_user_command("TSUpdateSync")
+vim.api.nvim_del_user_command("TSInstall")
+vim.api.nvim_del_user_command("TSInstallSync")
+vim.api.nvim_del_user_command("TSInstallFromGrammar")
+vim.api.nvim_del_user_command("TSUninstall")
+
+require("nvim-ts-autotag").setup {}
+
+require("mdx").setup {}
+
+require("treesitter-context").setup {
+  enable = true,
+  max_lines = 4,
+}
