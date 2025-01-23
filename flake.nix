b23ba4ef2eec6d0e12041adeccf2c3dda7d40af9
Ewan Gilchrist <egilchrist@kangaroot.net>
2025-01-23 14:59:01 +0100

    Add checks and pre-commit to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,30 +2,39 @@
   description = "Personal neovim wrapper";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      nvim = pkgs.callPackage ./neovim.nix {};
-    in {
-      # checks = {
-      #   # Run `nix flake check .` to verify that your config is not broken
-      #   default = nixvimLib.check.mkTestDerivationFromNvim {
-      #     inherit nvim;
-      #     name = "A nixvim configuration";
-      #   };
-      # };
-
-      packages = {
-        # Lets you run `nix run .` to start nixvim
-        default = nvim;
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }@inputs:
+    let
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./neovim.nix { };
+      });
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${pkgs.system}.pre-commit-check.enabledPackages;
+        };
+      });
+      checks = forAllSystems (pkgs: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${pkgs.system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-rfc-style.enable = true;
+          };
+        };
+      });
+    };
 }
